0950afec5adb46dfa5567ba5ce38be4c8bfd631b
kjq <kjq@qq.com>
2025-03-08 10:45:48 +0800

    初始化项目

---
+++ AIAudioTransformBackend/src/main/java/com/kjq/AiAudioTransformBackendApplication.java
@@ -0,0 +1,14 @@
+package com.kjq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AiAudioTransformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAudioTransformBackendApplication.class, args);
+    }
+
+}
